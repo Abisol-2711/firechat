2f5e2d07a729c8325c4a663f5322cadd2497407d
Sol <solabigail00@hotmail.com>
2025-09-03 23:06:58 -0300

    add zod and update register and login

--- src/pages/auth/login.page.tsx
+++ src/pages/auth/login.page.tsx
@@ -1,3 +1,10 @@
+import CardFooterAuth from '@/components/card-footer-auth'
+import { useAuthActions } from '@/hooks/use-auth-actions'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { loginZodSchema, type LoginZodSchemaType } from '@/lib/zod.schema'
+
 import {
   Card,
   CardContent,
@@ -5,12 +12,49 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
-
-import CardFooterAuth from '@/components/card-footer-auth'
-import { useAuthActions } from '@/hooks/use-auth-actions'
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { toast } from 'sonner'
 
 const LoginPage = () => {
-  const { loading } = useAuthActions()
+  const { loading, login } = useAuthActions()
+
+  const form = useForm<LoginZodSchemaType>({
+    resolver: zodResolver(loginZodSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  const onSubmit = async (data: LoginZodSchemaType) => {
+    const response = await login(data)
+    if (!response.success) {
+      console.log(response)
+
+      if (response.error?.code === 'auth/invalid-login-credentials') {
+        // form.setError('email', {
+        //   type: 'manual',
+        //   message: 'Invalid email or password',
+        // })
+
+        // form.setError('password', {
+        //   type: 'manual',
+        //   message: 'Invalid email or password',
+        // })
+
+        toast.error('Invalid email or password')
+      }
+    }
+  }
 
   return (
     <Card className="bg-white">
@@ -20,7 +64,45 @@ const LoginPage = () => {
           Sign in to your account or continue with Google
         </CardDescription>
       </CardHeader>
-      <CardContent>...</CardContent>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder="shadcn@example.com"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="••••••••" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full" disabled={loading}>
+              {loading ? 'Loading...' : 'Login'}
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
       <CardFooterAuth type="login" loading={loading} />
     </Card>
   )
--- src/pages/auth/register.page.tsx
+++ src/pages/auth/register.page.tsx
@@ -1,3 +1,6 @@
+import CardFooterAuth from '@/components/card-footer-auth'
+
+import { Button } from '@/components/ui/button'
 import {
   Card,
   CardContent,
@@ -5,22 +8,128 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
-
-import CardFooterAuth from '@/components/card-footer-auth'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
 import { useAuthActions } from '@/hooks/use-auth-actions'
+import { registerZodSchema, type RegisterZodSchemaType } from '@/lib/zod.schema'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 
 const RegisterPage = () => {
-  const { loading } = useAuthActions()
+  const { register, loading } = useAuthActions()
+
+  const form = useForm<RegisterZodSchemaType>({
+    resolver: zodResolver(registerZodSchema),
+    defaultValues: {
+      displayName: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+  })
+
+  const onSubmit = async (values: RegisterZodSchemaType) => {
+    const response = await register(values)
+    if (response.error) {
+      toast.error('Problemas al crear la cuenta')
+      console.log(response.error.code)
+      if (response.error.code === 'auth/email-already-in-use') {
+        form.setError('email', {
+          type: 'manual',
+          message: 'Email is already in use.',
+        })
+      } else {
+        console.error('Registration error:', response.error)
+      }
+    } else {
+      // Handle successful registration, e.g., redirect or show a success message
+      console.log('Registration successful', values)
+    }
+  }
 
   return (
     <Card className="bg-white">
       <CardHeader>
         <CardTitle>Register</CardTitle>
-        <CardDescription>
-          Sign up to your account or continue with Google
-        </CardDescription>
+        <CardDescription>Create a new account</CardDescription>
       </CardHeader>
-      <CardContent>...</CardContent>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="displayName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Display Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="Enter your display name" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="Enter your email" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      {...field}
+                      placeholder="Enter your password"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      {...field}
+                      placeholder="Confirm your password"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full" disabled={loading}>
+              Register
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
       <CardFooterAuth type="register" loading={loading} />
     </Card>
   )
